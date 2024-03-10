04b6e256d0dee302b2d8ea61b937f63e7ef1b41c
idfp <mail@foul.one>
2024-03-10 18:22:48 +0700

    home page almost done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,79 @@
 import Image from "next/image";
+import Link from "next/link"
+import { Key } from "lucide-react"
+import { Boxes } from "../components/ui/background-boxes";
+import { cn } from "@/lib/utils";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="h-screen relative w-full overflow-hidden bg-green-50 flex flex-col items-center justify-center ">
+      <div className="absolute inset-0 w-full h-full bg-green-50 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
+      <Boxes />
+      <div className="flex flex-row items-center content-center flex-wrap">
+
+        <div className="m-32 ABSOLUTEZINDEX">
+          <div className="bg-white z-20 m-8">
+            <Image src={"/upni.svg"} width={200} height={200} alt="UPN Insight Logo" className="z-20" style={{ zIndex: 20, color: "#000" }} />
+          </div>
+          <h1 className={cn("md:text-4xl text-xl text-black text-center z-20 ")}>
+            UPN Insight
+          </h1>
+          <p className="text-center mt-2 text-gray-700 z-20">
+            Talks, News, and Discussion
+          </p>
+        </div>
+        <div className="bg-white rounded-lg min-h-48 min-w-64 z-20 m-32 border-solid border border-gray-300">
+          <h1 className="text-center m-8 text-black text-xl md:text-2xl">Masuk ke Halaman Utama</h1>
+          <Tabs defaultValue="login" className="w-[400px] m-4">
+            <TabsList className="grid w-full grid-cols-2">
+              <TabsTrigger value="login">Login</TabsTrigger>
+              <TabsTrigger value="register">Register</TabsTrigger>
+            </TabsList>
+            <TabsContent value="login" className="px-8 py-2 inline-block">
+              <p className="text-sm text-gray-500 mb-4">
+                Masuk menggunakan email / NIM dan password.
+              </p>
+              <div className="space-y-1">
+                <Label htmlFor="id">Email / NIM</Label>
+                <Input id="id" placeholder="mahasiswa@upnv.ac.id" />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="password">Password</Label>
+                <Input id="password" type="password" placeholder="********" />
+              </div>
+              <Link href="/forgot" className="text-gray-500 text-xs">Forgot Password</Link>
+              <Button className="my-4" style={{ marginLeft: "70%" }}><Key className="mr-2 h-4 w-4" />Login</Button>
+            </TabsContent>
+            <TabsContent value="register" className="px-8 inline-block">
+              <p className="text-sm text-gray-500 mb-4 mt-0">
+                Buat akun baru di UPN Insight, kosongkan NIM apabila tidak memiliki.
+              </p>
+
+              <div className="space-y-1 mt-2">
+                <Label htmlFor="id">Email</Label>
+                <Input id="id" placeholder="mahasiswa@upnv.ac.id" />
+              </div>
+              <div className="space-y-1 mt-2">
+                <Label htmlFor="nim">NIM</Label>
+                <Input id="nim" placeholder="2310123456" />
+              </div>
+              <div className="space-y-1 mt-2">
+                <Label htmlFor="password">Password</Label>
+                <Input id="password" type="password" placeholder="********" />
+              </div>
+              <div className="space-y-1 mt-2">
+                <Label htmlFor="rpassword">Retype Password</Label>
+                <Input id="rpassword" type="password" placeholder="********" />
+              </div>
+              <Button className="my-4" style={{ marginLeft: "70%" }}><Key className="mr-2 h-4 w-4" />Register</Button>
+            </TabsContent>
+          </Tabs>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
